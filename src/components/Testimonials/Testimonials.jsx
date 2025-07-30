ce84a49480234fe85ae74e5dc9b93b48d477e002
saicharanbehara <saicharanbehara@gmail.com>
2025-07-30 15:18:13 +0530

    updated

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -6,14 +6,34 @@ import user1 from '../../assets/user-1.png'
 import user2 from '../../assets/user-2.png'
 import user3 from '../../assets/user-3.png'
 import user4 from '../../assets/user-4.png'
+import { useRef } from 'react'
 
 const Testimonials = () => {
+    const slider = useRef()
+
+    let tx= 0
+    
+    const slideForward = ()=>{
+        if (tx > -50)
+        {
+            tx -= 25;
+        }
+        slider.current.style.transform = `translateX(${tx}%)`
+    }
+    const slideBackward = ()=>{
+        if (tx < 0)
+        {
+            tx += 25;
+        }
+        slider.current.style.transform = `translateX(${tx}%)`
+    }
+
   return (
     <div className='Testimonials'>
-        <img src={next_icon} className='next-btn'/>
-        <img src={back_icon} className='back-btn'/>
+        <img src={next_icon} className='next-btn' onClick={slideForward}/>
+        <img src={back_icon} className='back-btn' onClick={slideBackward}/>
         <div className="slider">
-             <ul>
+             <ul ref={slider}>
                 <li>
                     <div className='slide'>
                         <div className="userInfo">
