345146cf290a33c5f61eb9dfcb5273e13893ff28
saicharanbehara <saicharanbehara@gmail.com>
2025-07-30 18:07:10 +0530

    Edusity

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import '../Navbar/Navbar.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   const[sticky,setSticky] = useState(false)
@@ -13,13 +14,37 @@ const Navbar = () => {
     <nav className={`container ${sticky ? 'darkNav' : '' }`}>
           <img src = {logo} alt = '' className='logo'/>
         <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About Us</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
             <li>
-                <button className='btn'> Contact us </button>
+              <Link to='Hero' smooth={true} offset={-250} duration={500}>
+              Home
+              </Link>
+            </li>
+            <li>
+              <Link to='program' smooth={true} offset={-250} duration={500}>
+              Program
+              </Link>
+              </li>
+            <li>
+              <Link to='about' smooth={true} offset={-250} duration={500}>
+              About Us
+              </Link>
+              </li>
+            <li>
+              <Link to='Campus' smooth={true} offset={-250} duration={500}>
+              Campus
+              </Link>
+              </li>
+            <li>
+              <Link to='Testimonials' smooth={true} offset={-250} duration={500}>
+               Testimonials
+              </Link>
+              </li>
+            <li>
+                <button className='btn'> 
+                  <Link to='Contact' smooth={true} offset={-250} duration={500}>
+               Contact Us
+              </Link>
+                   </button>
             </li>
         </ul>
     </nav>
